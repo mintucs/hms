c29f57736b55e8b94096bafd21cef973c7cea4a7
mintu <mintucs@users.noreply.github.com>
2015-07-02 12:35:29 +0530

    Update intialise-es.js

--- db-scripts/intialise-es.js
+++ db-scripts/intialise-es.js
@@ -23,22 +23,20 @@ create_promise.then(function(x) {
 
 var create_promise = client.indices.putMapping({"users": {
         "properties" : {
-            "username" : {"type" : "string",
-               "store" : true },
+            "USERNAME {"type" : "string", "store" : true },
+               "Password" : {"type" : "string""store" : true },
                
-               "access_tokens" : { "enabled" : true, "default" : "366"},
+               "access_tokens" : { "enabled" : true},
                
-               "tag": {
-        "type":     "string",
-        "index":    "not_analyzed"}
+               "tag": {"type":     "string", "index":    "not_analyzed"}
     }
         },
         "users" : {
-        "_ttl" : { "enabled" : true, }
+        "expire" : { "enabled" : true,"default" : "366" }
     }
     
 });
 create_promise.then(function(x) {
     console.log(x);
 }, function(err) { console.log(err);
-});
+}); 
